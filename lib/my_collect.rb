4453731029a159236ca26a9916c9beec0023a6ed
confident-token-1019 <theoonetj@gmail.com>
2019-12-18 06:14:43 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,5 +3,10 @@ def myCollectLab(array)
   i = 0 
   collectNames = []
   
-  while i < array.length
+  while i < array.length 
+  collectNames << yield([i])
+  i = i + 1 
+end 
+collectNames
+end
 
